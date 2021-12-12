749d153909e4c138bf89fc974cf657935b46ed0d
VairavanMuthu <vmuthu@express.com>
2021-12-12 17:39:07 +0530

    getEmployeeID added second time

--- src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
+++ src/main/java/com/howtodoinjava/rest/dao/EmployeeDAO.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 import com.howtodoinjava.rest.model.Employee;
 import com.howtodoinjava.rest.model.Employees;
 
+import java.util.stream.Collectors;
+
 @Repository
 public class EmployeeDAO 
 {
@@ -16,7 +18,13 @@ public class EmployeeDAO
         list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
         list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
     }
-    
+
+    public Employee getEmployeeById(int id)
+    {
+        Employee employee = list.getEmployeeList().stream().filter(a -> a.getId() == id).collect(Collectors.toList()).get(0);
+        return employee;
+    }
+
     public Employees getAllEmployees() 
     {
         return list;
